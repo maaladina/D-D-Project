a0ec5a3f6a324c95d2346f08ce8fce62cefc60ff
maaladina <maaladina@yandex.ru>
2024-06-22 00:16:40 +0400

    update

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,7 +6,7 @@ const Table = () => {
         <thead className="bg-gray-50 dark:bg-neutral-800">
           <tr>
 
-            <th scope="col" className="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3 text-start">
+            <th scope="col" className="px-6 py-3 text-start">
               <div className="flex items-center gap-x-2">
                 <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
                   Name
@@ -45,7 +45,7 @@ const Table = () => {
         <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
           <tr>
             <td className="size-px whitespace-nowrap">
-              <div className="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
+              <div className="px-6 py-3">
                 <div className="flex items-center gap-x-3">
                   <img className="inline-block size-[38px] rounded-full" src="https://images.unsplash.com/photo-1531927557220-a9e23c1e4794?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=htmlFormat&fit=facearea&facepad=2&w=300&h=300&q=80" alt="Image Description" />
                   <div className="grow">
@@ -55,7 +55,7 @@ const Table = () => {
                 </div>
               </div>
             </td>
-            <td className="h-px w-72 whitespace-nowrap">
+            <td className="size-px  whitespace-nowrap">
               <div className="px-6 py-3">
                 <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">Chris</span>
                 <span className="block text-sm text-gray-500 dark:text-neutral-500">GM</span>
@@ -73,7 +73,7 @@ const Table = () => {
                 <span className="text-sm text-gray-500 dark:text-neutral-500">Love playing D&D</span>
               </div>
             </td>
-            <td className="size-px whitespace-nowrap">
+            <td className="h-px w-72 whitespace-nowrap text-end">
               <div className="px-6 py-1.5">
                 <a className="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline font-medium dark:text-blue-500" href="#">
                   Edit
