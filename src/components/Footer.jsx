f20b8df9347a9eff2dd8d402c8f116a8e77da2cc
maaladina <maaladina@yandex.ru>
2024-06-21 23:49:58 +0400

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="px-6 py-4 grid gap-3 md:flex md:justify-between md:items-center border-t border-gray-200 dark:border-neutral-700">
         <div>
           <p className="text-sm text-gray-600 dark:text-neutral-400">
-            <span className="font-semibold text-gray-800 dark:text-neutral-200">12</span> results
+            <span className="font-semibold text-gray-800 dark:text-neutral-200">1</span> result
           </p>
         </div>
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,12 +5,6 @@ const Table = () => {
         <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
         <thead className="bg-gray-50 dark:bg-neutral-800">
           <tr>
-            <th scope="col" className="ps-6 py-3 text-start">
-              <label htmlFor="hs-at-with-checkboxes-main" className="flex">
-                <input type="checkbox" className="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-600 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="hs-at-with-checkboxes-main" />
-                <span className="sr-only">Checkbox</span>
-              </label>
-            </th>
 
             <th scope="col" className="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3 text-start">
               <div className="flex items-center gap-x-2">
@@ -23,7 +17,7 @@ const Table = () => {
             <th scope="col" className="px-6 py-3 text-start">
               <div className="flex items-center gap-x-2">
                 <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
-                  Position
+                  Nickname
                 </span>
               </div>
             </th>
@@ -31,7 +25,7 @@ const Table = () => {
             <th scope="col" className="px-6 py-3 text-start">
               <div className="flex items-center gap-x-2">
                 <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
-                  Status
+                  City
                 </span>
               </div>
             </th>
@@ -39,15 +33,7 @@ const Table = () => {
             <th scope="col" className="px-6 py-3 text-start">
               <div className="flex items-center gap-x-2">
                 <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
-                  Portfolio
-                </span>
-              </div>
-            </th>
-
-            <th scope="col" className="px-6 py-3 text-start">
-              <div className="flex items-center gap-x-2">
-                <span className="text-xs font-semibold uppercase tracking-wide text-gray-800 dark:text-neutral-200">
-                  Created
+                  About
                 </span>
               </div>
             </th>
@@ -58,14 +44,6 @@ const Table = () => {
 
         <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
           <tr>
-            <td className="size-px whitespace-nowrap">
-              <div className="ps-6 py-3">
-                <label htmlFor="hs-at-with-checkboxes-1" className="flex">
-                  <input type="checkbox" className="shrink-0 border-gray-300 rounded text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-600 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800" id="hs-at-with-checkboxes-1" />
-                  <span className="sr-only">Checkbox</span>
-                </label>
-              </div>
-            </td>
             <td className="size-px whitespace-nowrap">
               <div className="ps-6 lg:ps-3 xl:ps-0 pe-6 py-3">
                 <div className="flex items-center gap-x-3">
@@ -79,33 +57,20 @@ const Table = () => {
             </td>
             <td className="h-px w-72 whitespace-nowrap">
               <div className="px-6 py-3">
-                <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">Director</span>
-                <span className="block text-sm text-gray-500 dark:text-neutral-500">Human resources</span>
+                <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">Chris</span>
+                <span className="block text-sm text-gray-500 dark:text-neutral-500">GM</span>
               </div>
             </td>
             <td className="size-px whitespace-nowrap">
               <div className="px-6 py-3">
-                <span className="py-1 px-1.5 inline-flex items-center gap-x-1 text-xs font-medium bg-teal-100 text-teal-800 rounded-full dark:bg-teal-500/10 dark:text-teal-500">
-                  <svg className="size-2.5" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-                  </svg>
-                  Active
+                <span className="block text-sm font-semibold text-gray-800 dark:text-neutral-200">
+                  Batumi
                 </span>
               </div>
             </td>
             <td className="size-px whitespace-nowrap">
               <div className="px-6 py-3">
-                <div className="flex items-center gap-x-3">
-                  <span className="text-xs text-gray-500 dark:text-neutral-500">1/5</span>
-                  <div className="flex w-full h-1.5 bg-gray-200 rounded-full overflow-hidden dark:bg-neutral-700">
-                    <div className="flex flex-col justify-center overflow-hidden bg-gray-800 dark:bg-neutral-200" role="progressbar" style={{width: "25%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                  </div>
-                </div>
-              </div>
-            </td>
-            <td className="size-px whitespace-nowrap">
-              <div className="px-6 py-3">
-                <span className="text-sm text-gray-500 dark:text-neutral-500">28 Dec, 12:12</span>
+                <span className="text-sm text-gray-500 dark:text-neutral-500">Love playing D&D</span>
               </div>
             </td>
             <td className="size-px whitespace-nowrap">
